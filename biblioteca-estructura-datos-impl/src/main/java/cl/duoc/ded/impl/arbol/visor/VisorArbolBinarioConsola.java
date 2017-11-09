65d41501403572e0805fb86357b3cf6a633a65df
CETECOM <CETECOM@LC020920>
2017-11-09 19:46:50 -0300

    se añadio ejemplo para mostrar el imprimible como una imagen

--- biblioteca-estructura-datos-impl/src/main/java/cl/duoc/ded/impl/arbol/visor/VisorArbolBinarioConsola.java
+++ biblioteca-estructura-datos-impl/src/main/java/cl/duoc/ded/impl/arbol/visor/VisorArbolBinarioConsola.java
@@ -13,6 +13,14 @@ package cl.duoc.ded.impl.arbol.visor;
 import cl.duoc.ded.api.Arbol;
 import cl.duoc.ded.api.util.Visualizable;
 import cl.duoc.ded.exception.VisorException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JLabel;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+ 
 
 /**
  * Implementaci&oacute;n de la interfaz {@link cl.duoc.ded.api.util.Visualizable}
@@ -21,11 +29,40 @@ import cl.duoc.ded.exception.VisorException;
  * &lt;<a href="mailto:o.rodriguezv@profesor.duoc.cl">o.rodriguezv@profesor.duoc.cl</a>&gt;
  * @version 1.0
  */
-public class VisorArbolBinarioConsola implements Visualizable<Arbol> {
+public class VisorArbolBinarioConsola implements Visualizable<Arbol>  extends Jframe {
 
     @Override
-    public void ver(Arbol arbol) throws VisorException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void ver(Arbol arbol) throws VisorException{
+
+        String weasraras = new String("dauidhauisduidsaui");
+
+	JLabel etiqueta = new JLabel(weasraras);
+
+	public pruebaImagen()
+	{
+		super("Muestra de Imagen en JAVA...");
+ 
+		//AGREGAMOS LA ETIQUETA QUE CONTIENE LA IMAGEN AL FRAME
+		getContentPane().add(etiqueta);
+ 
+		//ESTABLECEMOS EL TAMAÑO DEL FRAME
+		this.setSize(500, 500);
+ 
+	}
+ 
+	public static void main(String [] args)
+	{
+		pruebaImagen p = new pruebaImagen();
+		p.show();
+ 
+		//COLOCAMOS EL CODIGO QUE PERMITE CERRAR LA VENTANA
+		p.addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent evt)
+			{
+				System.exit(0);
+			}
+		});
+	}    }
     
 }
